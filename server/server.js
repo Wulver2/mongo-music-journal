cfe60ea375434ed1da410590590d7253eb256fb1
Wulver2 <ross.shayla21@gmail.com>
2026-08-09 10:58:05 -0400

    Plan routes

--- server/server.js
+++ server/server.js
@@ -1,6 +1,19 @@
 import express from "express"
 
 const app = express()
+// Routes for music
+//create
+// add favorite songs/artists
+
+//get
+// get songs, artist with list of songs and albums, genres also shows related artists,
+// albums with list of songs from album, get a song (with lyrics?)
+
+// update
+// can update rating of a song/album
+
+// delete
+// remove artists/songs from favorites
 
 app.listen(5001, () => {
     console.log("Server on")
