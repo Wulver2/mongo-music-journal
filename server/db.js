70b11ec4d7c66ba363bdb44eb2a180dedbc69ab5
Wulver2 <ross.shayla21@gmail.com>
2026-08-10 09:52:45 -0400

    Create schema for songs

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ import 'dotenv/config';
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(`mongodb+srv://${process.env.DB_username}:${process.env.DB_password}@music-journal.pleozyg.mongodb.net/?appName=music-journal`);
+        await mongoose.connect(process.env.DB_url);
         console.log("db connected")
     } catch (error) {
         console.error(error.message)
---
+++ server/models/song.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+// may add rating 
+const songSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    artist: {
+        type: String,
+        required: true
+    },
+    album: {
+        type: String
+    },
+    tags: {
+        type:[String]
+    },
+    lyrics: {
+        type: String
+    }
+});
