64dff1f8e0d45902cf4fbc3adda08654c3969a91
Wulver2 <ross.shayla21@gmail.com>
2026-08-16 15:48:04 -0400

    Insert base album and song data need to get artist and album id for album and songs

--- server/seed/seed.js
+++ server/seed/seed.js
@@ -4,6 +4,7 @@ import Artist from "../models/artist";
 import Album from "../models/album";
 import Song from "../models/song";
 import musicData from "./data";
+import { json } from "express";
 
 
 const seedDB = async () => {
@@ -18,6 +19,24 @@ const seedDB = async () => {
     // album and songs can use there ids to refrence them
     for (var i = 0; i < musicData.length(); i++) {
         await Artist.insertOne({name: musicData[i].name, tags: musicData[i].tags})
+
+
+    }
+
+    for (var i = 0; i < musicData.albums.length(); i++) {
+        await Album.insertOne({
+            title: musicData.albums[i].title,
+            releaseDate: musicData.albums[i].releaseDate,
+            //artist: 
+        });
+
+        for (var j = 0; j < musicData.albums[i].songs.length(); j++) {
+            await Song.insertOne({
+                name: musicData.albums[i].songs[j].name,
+                //artist: 
+                //album: 
+            });
+        }
     }
 
 }
