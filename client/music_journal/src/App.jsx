dc153f4cd3b694e0c4b0c73cf22ec33f6d7e7731
Wulver2 <ross.shayla21@gmail.com>
2026-08-24 11:05:44 -0400

    Create persistent navigation bar

--- client/music_journal/src/App.jsx
+++ client/music_journal/src/App.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Routes, Route} from "react-router"
 import { Login } from './pages/login'
 import { Register } from './pages/register'
 import { Home } from './pages/home'
+import { Navbar } from './components/navbar'
 import './App.css'
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Navbar></Navbar>
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/login' element={<Login/>}/>
--- client/music_journal/src/components/navbar.jsx
+++ client/music_journal/src/components/navbar.jsx
@@ -1,5 +1,12 @@
+import { Link } from "react-router";
 
 
 export function Navbar({ isLoggedIn = false }) {
-
+    return (
+        <>
+            <Link to="/"> Home </Link>
+            <Link to="/login"> Login </Link>
+            <Link to="/register"> Register </Link>
+        </>
+    )
 }
