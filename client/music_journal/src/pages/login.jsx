3d674ac9380a842af97ef4080b11204429ebc7da
Wulver2 <ross.shayla21@gmail.com>
2026-08-24 11:20:20 -0400

    Set form for logging in

--- client/music_journal/src/pages/login.jsx
+++ client/music_journal/src/pages/login.jsx
@@ -1,12 +1,31 @@
+import { useState } from "react"
 
 
 export function Login() {
-    const [email, setEmail]
+    const [form, setForm] = useState({
+        email: "",
+        password: ""
+    });
+
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+    }
+
     return (
         <>
             <h1>login</h1>
             <form action="">
-                <input type="email"/>
+                <label htmlFor="email"> Email: </label>
+                <input type="email" id="email" onChange={
+                    (e) => {
+                        setForm({ ...form, email: e.target.value })
+                    }} />
+                <label htmlFor="password"> Password: </label>
+                <input type="password" id="password" onChange={
+                    (e) => {
+                        setForm({ ...form, password: e.target.value })
+                    }} />
+                <button type="submit">Log in</button>
             </form>
         </>
     )
