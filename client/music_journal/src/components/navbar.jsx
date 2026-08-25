9d20532f18d895167b61059a09ceddf7ecae47e4
Wulver2 <ross.shayla21@gmail.com>
2026-08-25 10:03:18 -0400

    Move navbar to left of screen

--- client/music_journal/src/components/navbar.jsx
+++ client/music_journal/src/components/navbar.jsx
@@ -4,10 +4,12 @@ import { Link } from "react-router";
 export function Navbar({ isLoggedIn = false }) {
     return (
         <>
-            <Link to="/"> Home </Link>
-            <Link to="/login"> Login </Link>
-            <Link to="/register"> Register </Link>
-            <button class="bg-slate-900 text-white hover:bg-slate-600">dark mode</button>
+            <div className="fixed top-0 h-screen w-16 m-0 flex flex-col bg-gray-800 shadow-lg text-white">
+                <Link to="/"> Home </Link>
+                <Link to="/login"> Login </Link>
+                <Link to="/register"> Register </Link>
+                <button className="bg-slate-900 hover:bg-slate-600">dark mode</button>
+            </div>
         </>
     )
 }
