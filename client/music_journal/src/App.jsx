18950d1f280aa0059aff9e261b2a2588906644c8
Wulver2 <ross.shayla21@gmail.com>
2026-08-25 09:55:36 -0400

    Install tailwindcss

--- client/music_journal/src/App.jsx
+++ client/music_journal/src/App.jsx
@@ -4,7 +4,6 @@ import { Login } from './pages/login'
 import { Register } from './pages/register'
 import { Home } from './pages/home'
 import { Navbar } from './components/navbar'
-import './App.css'
 
 function App() {
   return (
--- client/music_journal/src/components/navbar.jsx
+++ client/music_journal/src/components/navbar.jsx
@@ -7,6 +7,7 @@ export function Navbar({ isLoggedIn = false }) {
             <Link to="/"> Home </Link>
             <Link to="/login"> Login </Link>
             <Link to="/register"> Register </Link>
+            <button class="bg-slate-900 text-white hover:bg-slate-600">dark mode</button>
         </>
     )
 }
