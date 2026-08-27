a4d0ebd9efe885f0c5ef13413e05a7ff66b48a85
Wulver2 <ross.shayla21@gmail.com>
2026-08-27 09:03:29 -0400

    More login style changes

--- client/music_journal/src/pages/login.jsx
+++ client/music_journal/src/pages/login.jsx
@@ -15,13 +15,15 @@ export function Login() {
             <h1>login</h1>
             <form className="
             fixed flex flex-col left-2/5 outline-solid rounded-sm shadow-2xl
-            bg-gray-900 text-white p-8
+            bg-gray-900 text-white p-8 gap-2
             ">
                 <label htmlFor="email"> Email: </label>
-                <input type="email" id="email" placeholder="email" onChange={
-                    (e) => {
-                        setForm({ ...form, email: e.target.value })
-                    }} />
+                <input type="email" id="email" placeholder="email"
+                    className="outline-solid rounded-sm outline-gray-5"
+                    onChange={
+                        (e) => {
+                            setForm({ ...form, email: e.target.value })
+                        }} />
                 <label htmlFor="password"> Password: </label>
                 <input type="password" placeholder="password" id="password"
                     className="outline-solid rounded-sm outline-gray-50"
@@ -29,7 +31,7 @@ export function Login() {
                         (e) => {
                             setForm({ ...form, password: e.target.value })
                         }} />
-                <button type="submit">Log in</button>
+                <button type="submit" className="pl-24  pr-24 bg-green-600 self-center rounded-sm hover:bg-green-700">Log in</button>
             </form>
         </>
     )
