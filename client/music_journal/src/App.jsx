982c3786990dd26ec7aac80d878e6f37cb88ddbc
Wulver2 <ross.shayla21@gmail.com>
2026-08-27 08:38:54 -0400

    Apply dark background to all pages and to login form

--- client/music_journal/src/App.jsx
+++ client/music_journal/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter, Routes, Route} from "react-router"
+import { BrowserRouter, Routes, Route } from "react-router"
 import { Login } from './pages/login'
 import { Register } from './pages/register'
 import { Home } from './pages/home'
@@ -8,15 +8,16 @@ import { Navbar } from './components/navbar'
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Navbar></Navbar>
-        <Routes>
-          <Route path='/' element={<Home/>} />
-          <Route path='/login' element={<Login/>}/>
-          <Route path='/Register' element={<Register/>}/>
-        </Routes>
-      </BrowserRouter>
-
+      <div className='bg-neutral-900 h-screen'>
+        <BrowserRouter>
+          <Navbar></Navbar>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/Register' element={<Register />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </>
   )
 }
--- client/music_journal/src/pages/login.jsx
+++ client/music_journal/src/pages/login.jsx
@@ -13,7 +13,10 @@ export function Login() {
     return (
         <>
             <h1>login</h1>
-            <form className="fixed flex flex-col left-2/5 outline-solid rounded-sm shadow-2xl">
+            <form className="
+            fixed flex flex-col left-2/5 outline-solid rounded-sm shadow-2xl
+            bg-gray-900 text-white p-8
+            ">
                 <label htmlFor="email"> Email: </label>
                 <input type="email" id="email" placeholder="email" onChange={
                     (e) => {
@@ -21,7 +24,7 @@ export function Login() {
                     }} />
                 <label htmlFor="password"> Password: </label>
                 <input type="password" placeholder="password" id="password"
-                    className="outline-solid rounded-sm"
+                    className="outline-solid rounded-sm outline-gray-50"
                     onChange={
                         (e) => {
                             setForm({ ...form, password: e.target.value })
