1f104c41b76c934c3e597b6df3d9b5f6bca2c624
Wulver2 <ross.shayla21@gmail.com>
2026-08-29 12:31:21 -0400

    Link register to page to login page, if they already have an acount and styled page

--- client/music_journal/src/pages/register.jsx
+++ client/music_journal/src/pages/register.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { Link } from "react-router";
 
 export function Register() {
     const [form, setForm] = useState({
@@ -14,26 +15,35 @@ export function Register() {
     return (
         <>
             <h1>login</h1>
-            <form action="">
+            <form className="fixed flex flex-col left-2/5 outline-solid outline-gray-900 rounded-sm shadow-2xl
+            bg-gray-900 text-white p-8 gap-2">
                 <label htmlFor="email"> Email: </label>
-                <input type="email" id="email" onChange={
-                    (e) => {
-                        setForm({ ...form, email: e.target.value })
-                    }} />
+                <input type="email" id="email"
+                    className="outline-solid rounded-sm outline-gray-5"
+                    onChange={
+                        (e) => {
+                            setForm({ ...form, email: e.target.value })
+                        }} />
                 <label htmlFor="username"> Username: </label>
-                <input type="text" name="username" id="username" onChange={
-                    (e) => {
-                        setForm({ ...form, username: e.target.value })
-                    }
-                } />
+                <input type="text" name="username" id="username"
+                    className="outline-solid rounded-sm outline-gray-5"
+                    onChange={
+                        (e) => {
+                            setForm({ ...form, username: e.target.value })
+                        }
+                    } />
                 <label htmlFor="password"> Password: </label>
-                <input type="password" id="password" onChange={
-                    (e) => {
-                        setForm({ ...form, password: e.target.value })
-                    }} />
+                <input type="password" id="password"
+                    className="outline-solid rounded-sm outline-gray-5"
+                    onChange={
+                        (e) => {
+                            setForm({ ...form, password: e.target.value })
+                        }} />
                 <label htmlFor="confirmPassword"> Confirm password: </label>
-                <input type="confirmPassword" id="confirmPassword" on />
-                <button type="submit"> Register</button>
+                <input type="confirmPassword" id="confirmPassword"
+                    className="outline-solid rounded-sm outline-gray-5" />
+                <button type="submit" className="pl-28  pr-28 bg-green-600 self-center rounded-sm hover:bg-green-700"> Register</button>
+                <p>Have an account already? <Link to="/login" className="underline text-blue-400">Log in</Link></p>
             </form>
         </>
     )
