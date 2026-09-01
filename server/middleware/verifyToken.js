621f3f6702775e8dc2d3d10811dd7505b397df8b
Wulver2 <ross.shayla21@gmail.com>
2026-09-01 09:55:10 -0400

    Verifes session Tokens, to be used as middleware for certain backend routes

--- server/middleware/verifyToken.js
+++ server/middleware/verifyToken.js
@@ -1,6 +1,18 @@
+import jwt from "jsonwebtoken";
+import "dotenv/config";
+
 export const verifyToken = (req, res, next) => {
     try {
-        
+        const sessionToken = req.cookies.sessionToken;
+        jwt.verify(sessionToken, process.env.JWT_SECRET, function(err, decoded) {
+            if (err) {
+                res.status(401).json({message: "Not a valid token"});
+                // Frontend should send them back to login
+            }
+            else {
+                next()
+            }
+        })
     } catch (error) {
         console.log(error.message);
     }
