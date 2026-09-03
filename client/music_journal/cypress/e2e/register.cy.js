17abce409b6dc118f111ced25ec63796f6653e1b
Wulver2 <ross.shayla21@gmail.com>
2026-09-03 10:00:36 -0400

    Plan registration tests

---
+++ client/music_journal/cypress/e2e/register.cy.js
@@ -0,0 +1,11 @@
+describe('Registration', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:5173/register');
+  })
+  // one test for sucessful login (goes to dash board)
+  // one for incorrect confirm password
+  //email
+  //username
+  //password
+  //confirm password
+})
