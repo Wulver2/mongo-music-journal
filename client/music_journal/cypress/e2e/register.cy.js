f391c852bce92579a22dc679aa321cd3de2a5ab9
Wulver2 <ross.shayla21@gmail.com>
2026-09-04 10:20:55 -0400

    Set up test for passing registration

--- client/music_journal/cypress/e2e/register.cy.js
+++ client/music_journal/cypress/e2e/register.cy.js
@@ -1,19 +1,28 @@
 /// <reference types="Cypress" />
 
 describe('Registration', () => {
-  beforeEach( () => {
-    cy.visit('http://localhost:5173/register');
-  });
-  // one test for sucessful login (goes to dash board)
-  // one for incorrect confirm password
-  it("user enters incorrect confirm password", () => {
-    //email
-    cy.get("#email").type("test@example.com");
-    cy.get("#username").type("test@example.com");
-    cy.get("#password").type("password123");
-    cy.get("#confirmPassword").type("123password");
-    cy.get('#regiButton').click()
-    cy.url().should('eq','http://localhost:5173/register')
-    cy.get("#matchError").should('contain', "Passwords do not match");
-  })
+    beforeEach(() => {
+        cy.visit('http://localhost:5173/register');
+    });
+    // one test for sucessful login (goes to dash board)
+    // one for incorrect confirm password
+    it("user enters incorrect confirm password", () => {
+        //email
+        cy.get("#email").type("test@example.com");
+        cy.get("#username").type("test@example.com");
+        cy.get("#password").type("password123");
+        cy.get("#confirmPassword").type("123password");
+        cy.get('#regiButton').click()
+        cy.url().should('eq', 'http://localhost:5173/register')
+        cy.get("#matchError").should('contain', "Passwords do not match");
+    })
+    it("user successfully registers", () => {
+        cy.get("#email").type("test@example.com");
+        cy.get("#username").type("test@example.com");
+        cy.get("#password").type("password123");
+        cy.get("#confirmPassword").type("password123");
+        //cy.get('#regiButton').click()
+        //cy.url().should('eq', 'http://localhost:5173/')
+    })
+    // delete user so that test can be used again -> have to add delete user route
 })
