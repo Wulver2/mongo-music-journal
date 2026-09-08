c7728250a36a2795659b525306f486dbbd26771b
Wulver2 <ross.shayla21@gmail.com>
2026-09-08 10:10:27 -0400

    Add artists and albums to favorites

--- server/routes/favorites.js
+++ server/routes/favorites.js
@@ -2,6 +2,8 @@ import express from "express"
 import verifyToken from "../middleware/verifyToken";
 import User from "../models/user";
 import Song from "../models/song";
+import Album from "../models/album";
+import Artist from "../models/artist";
 
 export const router = express.Router("express");
 
@@ -22,10 +24,30 @@ router.post("/song", verifyToken, async (req, res) => {
 });
 
 router.post("/artist", verifyToken, async (req, res) => {
-    const { artist } = req.params;
+    try {
+        const { artist, email } = req.params;
+        const user = await User.findOne({ email: email });
+        const artistId = await Artist.findOne(artist)._id;
+
+        user.favoriteArtists.push(artistId);
+        await user.save();
+    }
+    catch (error) {
+        console.error(error.message);
+    }
 });
-router.post("/albums", verifyToken, async (req, res) => {
-    const { albums } = req.params;
+router.post("/album", verifyToken, async (req, res) => {
+    try {
+        const { album, email } = req.params;
+        const user = await User.findOne({ email: email });
+        const albumId = await Album.findOne(album)._id;
+
+        user.favoriteAlbums.push(albumId);
+        await user.save();
+    }
+    catch (error) {
+        console.error(error.message);
+    }
 });
 
 router.delete("/artist/:artist", verifyToken, async (req, res) => {
