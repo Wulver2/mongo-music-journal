1fc8b9568b84405179d954b9ff7902ecf6668970
Wulver2 <ross.shayla21@gmail.com>
2026-09-08 10:20:47 -0400

    Remove artists from favorites

--- server/routes/favorites.js
+++ server/routes/favorites.js
@@ -1,18 +1,20 @@
 import express from "express"
-import verifyToken from "../middleware/verifyToken";
-import User from "../models/user";
-import Song from "../models/song";
-import Album from "../models/album";
-import Artist from "../models/artist";
+import verifyToken from "../middleware/verifyToken.js";
+import User from "../models/user.js";
+import Song from "../models/song.js";
+import Album from "../models/album.js";
+import Artist from "../models/artist.js";
 
 export const router = express.Router("express");
 
-// add favorite songs/artists
+// add favorite songs/artists 
+// TODO:// prevent song, artists, etc from being added multiple times
 router.post("/song", verifyToken, async (req, res) => {
     try {
         const { song, email } = req.params;
         const user = await User.findOne({ email: email });
         const songId = await Song.findOne(song)._id;
+        // check if id is already in favorite songs
 
         user.favoriteSongs.push(songId);
         await user.save();
@@ -50,8 +52,30 @@ router.post("/album", verifyToken, async (req, res) => {
     }
 });
 
-router.delete("/artist/:artist", verifyToken, async (req, res) => {
+router.get("/songs", verifyToken, async (req, res) => {
+    try {
+        const { email } = req.params;
+        const user = await User.findOne({ email: email });
 
+        for (var i = 0; i < length(user.favoriteSongs); i++) {
+
+        }
+    } catch (error) {
+        console.error(error.message)
+    }
+})
+
+router.delete("/artist/:artist", verifyToken, async (req, res) => {
+    try {
+        const { email } = req.params;
+        const user = await User.findOne({ email: email});
+        const artistId = await Artist.findOne(artist)._id;
+
+        user.favoriteArtists.pull(artistId)
+        await user.save();
+    } catch (error) {
+        console.error(error.message);
+    }
 });
 
 router.delete("/song/:song", verifyToken, async (req, res) => {
