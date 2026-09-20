e778c4a7c267bfb7edeaa5c0830724fda15a9fe6
Wulver2 <ross.shayla21@gmail.com>
2026-09-20 09:58:50 -0400

    Create Favorites page

---
+++ client/music_journal/src/pages/favorites.jsx
@@ -0,0 +1,12 @@
+
+export function Favorites() {
+
+    const getFavorites = async() => {
+
+    }
+    return (
+        <div className="text-white">
+            <h1>Favorites</h1>
+        </div>
+    )
+}
