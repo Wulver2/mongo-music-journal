911eafa3936df4e0be5bfe9ea359657f57a24238
Wulver2 <ross.shayla21@gmail.com>
2026-09-21 08:58:15 -0400

    Use state to hold favorite songs, albums, and artists

--- client/music_journal/src/pages/favorites.jsx
+++ client/music_journal/src/pages/favorites.jsx
@@ -1,12 +1,22 @@
+import axios from "axios"
+import { useState } from "react"
 
 export function Favorites() {
-
+    const [favorites, setFavorites] = useState[{
+        songs: [],
+        albums: [],
+        artists: []
+    }]
     const getFavorites = async() => {
-
+        const songs = await axios.get("https:/localhost:5001/favorites/songs")
+        setFavorites({...Favorites, songs: songs.data});
     }
     return (
         <div className="text-white">
             <h1>Favorites</h1>
+            <div>
+                {}
+            </div>
         </div>
     )
 }
